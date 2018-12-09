2f0d9b0e06a244ecba18da29cde5fda635c7449a
Beakyewon <43024179+Beakyewon@users.noreply.github.com>
2018-12-10 03:38:13 +0900

    10

--- movie.c
+++ movie.c
@@ -3,120 +3,44 @@
 #include <string.h>
 #include "movie.h"
 
-typedef struct movInfo{ //구조체 
+struct movInfo{ //구조체 
 	char name[200];
 	float score;
 	int runTime;
 	char madeIn[10];
 	
-} movInfo_t;
-
+};
 
 void* mv_genMvInfo(char* name, float score, int runTime, char* country)
 {
 	//구조체 포인터  
+	struct movInfo movInfo_t[10];
 	
-	struct movInfo_t[10];
 	int i;
-	movInfo_t[i] *mvPtr[i]; 
 	
-	
-	
+	struct movInfo *mvPtr;
+
 	//allocate memory and set the member variables 메모리 할당. 변수 설정.  
-	
-	mvPtr = (movInfo_t*) malloc(sizeof(movInfo_t));
-	
+	mvPtr = (struct movInfo*) malloc(sizeof(struct movInfo));
+
 	mvPtr->name = name; 
 	mvPtr->score = score;
 	mvPtr->runTime = runTime;
 	mvPtr->madeIn = country;
 	
+
 	for(i=0; i<10; i++){
 	 
- 	fscanf(fp, "%s %s %i %f", mvPtr[i]->name, mvPtr[i]->country, mvPtr[i]->runTime, mvPtr[i]->score);
+ 		fscanf(fp, "%s %s %i %f", (mvPtr+i)->name, (mvPtr+i)->madeIn, &(mvPtr+i)->runTime, &(mvPtr+i)->score);
  	
+	 	printf("%s %s %i %f \n", (mvPtr+i)->name, (mvPtr+i)->madeIn, (mvPtr+i)->runTime, (mvPtr+i)->score); 
 	 }
-	
-	free(mvPtr);
-/*
-	
-	char *p=NULL, *cut"\t", str[500];
-	
-		p = strstr(str, cut); //srt의 문자열에서 cut 문자열을 찾는다. 찾으면 str에서 찾은 문자열의 시작주소가 반환된다. 
-		strcpy(mvPtr->name, p);
-		
-		while((p = strtok(NULL, cut)) != NULL); //strtok 문자열 분리 함수 
-		{
-			i++;
-			switch(i){
-				case 1 :
-					strcpy(mvPtr->madeIn,p); //strcpy(to, from) from의 문자열이 to에 복사된다. 배열의 경계를 검사하지 않는다. 
-					break;
-				case 2 :
-					mvPtr->runTime = atoi(p);
-					break;
-				case 3 :
-					mvPtr->score = atoi(p);
-					break;	
-				default :
-					break;
-			}
-			if(i<3) break;
-		}
-	
-
-	return (void*)mvPtr;  //main 함수에서 파일속 문자열 읽어서 mv_genMvInfo이용해 구조체에 저장.  
-}	
-	
-	char *p= NULL, *cut="\t", str[500];
-	int i=0, n=0;
-	
-	
-	headPtr = malloc(sizeof(movInfo));
-	memset(headPtr, 0, sizeof(movInfo));
-	prePtr= mvPtr= endPtr= headPtr;
-	
-	while(1){
-		i=0;
-		fgets(str, 500, fp);
-		if(feof(fp) !=0){
-			if(n==0){
-				free(headPtr);
-				headPtr = endPtr =NULL;
-			}
-			else{
-				prePtr->next =NULL;
-				endPtr = prePtr;
-				free( mvPtr);
-				mv_print();
-			}
-			return n;
-		}
-		
-		p = strstr(str, cut); srt의 문자열에서 cut 문자열을 찾는다. 찾으면 str에서 찾은 문자열의 시작주소가 반환된다. 
-		strcpy(mvPtr->name, p);
-		
-		while((p = strtok(NULL, cut)) != NULL);
-		{
-			i++;
-			switch(i){
-				case 1 :
-					strcpy(mvPtr->madeIn,p);
-					break;
-				case 2 :
-					mvPtr->runTime = atoi(p);
-					break;
-				case 3 :
-					mvPtr->score = atoi(p);
-					break;	
-				default :
-					break;
-			}
-			if(i<3) break;
-		}
-	}*/
+	 
+	return (void*) mvPtr;
+}
 
 
+	
 
 void mv_print(void* obj)
 {
@@ -128,7 +52,7 @@ void mv_print(void* obj)
 	}
 	
 	printf("Name : %s (%s)\n", mvPtr->name, mvPtr->madeIn);
-	printf("running time : %i, score : %f\n", mvPtr->runTime, mvPtr->score);
+	printf("running time : %d, score : %f\n", mvPtr->runTime, mvPtr->score);
 	
 	return;
 }
